a0023cd53c3ba38b4c420706824a3d5f6c1f9fad
Evg <budo@narod.ru>
2019-03-18 01:01:08 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,6 +8,13 @@ register_asset "stylesheets/portal.scss"
 after_initialize do
 
   # add info category
+  Site.preloaded_category_custom_fields << "categorization" if Site.respond_to? :preloaded_category_custom_fields 
+  Site.preloaded_category_custom_fields << "categorqa" if Site.respond_to? :preloaded_category_custom_fields 
+  Site.preloaded_category_custom_fields << "categorimg" if Site.respond_to? :preloaded_category_custom_fields 
+  Site.preloaded_category_custom_fields << "categordiscr" if Site.respond_to? :preloaded_category_custom_fields 
+  Site.preloaded_category_custom_fields << "categortxt" if Site.respond_to? :preloaded_category_custom_fields 
+  
+  
   add_to_serializer(:basic_category, :categorization, false) { object.custom_fields['categorization'] }
   add_to_serializer(:basic_category, :categorqa, false) { object.custom_fields['categorqa'] }
   add_to_serializer(:basic_category, :categorimg, false) { object.custom_fields['categorimg'] }
